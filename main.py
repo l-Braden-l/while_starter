d42c0d5d5d86112012f32a8cba7ff00366579c4a
braden <leachbr787@northwested.org>
2024-11-26 14:12:28 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#Braden Leach
+#Nov 26 2024
+#While Loop Practice
+
+
+
+ 
+number = 0
+tempatures = []
+
+while number != -9999:
+ 
+ number = int(input(f'Enter a temerature in Fahrenheit (-9999 to quit): '))
+ tempatures.append(number)
+
+
+tempatures.pop()
+
+list_length = len(tempatures)
+if list_length > 0:
+   print(f'Temeratures entered: {tempatures}')
+   average = sum(tempatures) / len(tempatures)
+   print(f'average temperature:{average:.2f}°F')   
+else:
+    print('No temeratures were entered')
